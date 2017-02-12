3d90b38ddaa684461d6b0f2229e9842eb5f220dc
jcsme <jcsme@DESKTOP-O6725QL>
2017-02-12 13:28:19 -0500

    Add project files.

---
+++ SelectionIfStatement/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace SelectionIfStatement
+{
+    partial class SelectionIfStatement
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnDualAlt = new System.Windows.Forms.Button();
+            this.btnTryParse = new System.Windows.Forms.Button();
+            this.btnMultiAlt = new System.Windows.Forms.Button();
+            this.btnDriverLic = new System.Windows.Forms.Button();
+            this.btnCoupon = new System.Windows.Forms.Button();
+            this.btnVending = new System.Windows.Forms.Button();
+            this.btnTicket = new System.Windows.Forms.Button();
+            this.btnSingleAlt = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(558, 261);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 46);
+            this.btnClear.TabIndex = 0;
+            this.btnClear.Text = "CLEA&R";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(558, 328);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 46);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "E&XIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnDualAlt
+            // 
+            this.btnDualAlt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDualAlt.Location = new System.Drawing.Point(159, 257);
+            this.btnDualAlt.Name = "btnDualAlt";
+            this.btnDualAlt.Size = new System.Drawing.Size(118, 50);
+            this.btnDualAlt.TabIndex = 2;
+            this.btnDualAlt.Text = "Dual Alternatives";
+            this.btnDualAlt.UseVisualStyleBackColor = true;
+            this.btnDualAlt.Click += new System.EventHandler(this.btnDualAlt_Click);
+            // 
+            // btnTryParse
+            // 
+            this.btnTryParse.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTryParse.Location = new System.Drawing.Point(159, 324);
+            this.btnTryParse.Name = "btnTryParse";
+            this.btnTryParse.Size = new System.Drawing.Size(118, 50);
+            this.btnTryParse.TabIndex = 3;
+            this.btnTryParse.Text = "Try Parse";
+            this.btnTryParse.UseVisualStyleBackColor = true;
+            this.btnTryParse.Click += new System.EventHandler(this.btnTryParse_Click);
+            // 
+            // btnMultiAlt
+            // 
+            this.btnMultiAlt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMultiAlt.Location = new System.Drawing.Point(294, 257);
+            this.btnMultiAlt.Name = "btnMultiAlt";
+            this.btnMultiAlt.Size = new System.Drawing.Size(118, 50);
+            this.btnMultiAlt.TabIndex = 4;
+            this.btnMultiAlt.Text = "Mutiple Alternatives";
+            this.btnMultiAlt.UseVisualStyleBackColor = true;
+            this.btnMultiAlt.Click += new System.EventHandler(this.btnMultiAlt_Click);
+            // 
+            // btnDriverLic
+            // 
+            this.btnDriverLic.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDriverLic.Location = new System.Drawing.Point(294, 324);
+            this.btnDriverLic.Name = "btnDriverLic";
+            this.btnDriverLic.Size = new System.Drawing.Size(118, 50);
+            this.btnDriverLic.TabIndex = 5;
+            this.btnDriverLic.Text = "Driver License";
+            this.btnDriverLic.UseVisualStyleBackColor = true;
+            this.btnDriverLic.Click += new System.EventHandler(this.btnDriverLic_Click);
+            // 
+            // btnCoupon
+            // 
+            this.btnCoupon.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCoupon.Location = new System.Drawing.Point(418, 257);
+            this.btnCoupon.Name = "btnCoupon";
+            this.btnCoupon.Size = new System.Drawing.Size(118, 50);
+            this.btnCoupon.TabIndex = 6;
+            this.btnCoupon.Text = "Coupon";
+            this.btnCoupon.UseVisualStyleBackColor = true;
+            this.btnCoupon.Click += new System.EventHandler(this.btnCoupon_Click);
+            // 
+            // btnVending
+            // 
+            this.btnVending.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVending.Location = new System.Drawing.Point(418, 324);
+            this.btnVending.Name = "btnVending";
+            this.btnVending.Size = new System.Drawing.Size(118, 50);
+            this.btnVending.TabIndex = 7;
+            this.btnVending.Text = "Vending Machine";
+            this.btnVending.UseVisualStyleBackColor = true;
+            this.btnVending.Click += new System.EventHandler(this.btnVending_Click);
+            // 
+            // btnTicket
+            // 
+            this.btnTicket.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTicket.Location = new System.Drawing.Point(23, 324);
+            this.btnTicket.Name = "btnTicket";
+            this.btnTicket.Size = new System.Drawing.Size(118, 50);
+            this.btnTicket.TabIndex = 8;
+            this.btnTicket.Text = "Ticket";
+            this.btnTicket.UseVisualStyleBackColor = true;
+            this.btnTicket.Click += new System.EventHandler(this.btnTicket_Click);
+            // 
+            // btnSingleAlt
+            // 
+            this.btnSingleAlt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSingleAlt.Location = new System.Drawing.Point(23, 257);
+            this.btnSingleAlt.Name = "btnSingleAlt";
+            this.btnSingleAlt.Size = new System.Drawing.Size(118, 50);
+            this.btnSingleAlt.TabIndex = 9;
+            this.btnSingleAlt.Text = "Single Alternative";
+            this.btnSingleAlt.UseVisualStyleBackColor = true;
+            this.btnSingleAlt.Click += new System.EventHandler(this.btnSingleAlt_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(23, 38);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(610, 207);
+            this.txtOutput.TabIndex = 10;
+            // 
+            // SelectionIfStatement
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 385);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnSingleAlt);
+            this.Controls.Add(this.btnTicket);
+            this.Controls.Add(this.btnVending);
+            this.Controls.Add(this.btnCoupon);
+            this.Controls.Add(this.btnDriverLic);
+            this.Controls.Add(this.btnMultiAlt);
+            this.Controls.Add(this.btnTryParse);
+            this.Controls.Add(this.btnDualAlt);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Name = "SelectionIfStatement";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnDualAlt;
+        private System.Windows.Forms.Button btnTryParse;
+        private System.Windows.Forms.Button btnMultiAlt;
+        private System.Windows.Forms.Button btnDriverLic;
+        private System.Windows.Forms.Button btnCoupon;
+        private System.Windows.Forms.Button btnVending;
+        private System.Windows.Forms.Button btnTicket;
+        private System.Windows.Forms.Button btnSingleAlt;
+        private System.Windows.Forms.TextBox txtOutput;
+    }
+}
+
---
+++ SelectionIfStatement/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace SelectionIfStatement
+{
+    public partial class SelectionIfStatement : Form
+    {
+        public SelectionIfStatement()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtOutput.Text = "";
+            txtOutput.BackColor = Color.White;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSingleAlt_Click(object sender, EventArgs e)
+        {
+            string strUser = Interaction.InputBox(
+                "Do you want to turn the box orange? Y for Yes, other letters for No.");
+            if (strUser.ToUpper() == "Y")
+            {
+                txtOutput.BackColor = Color.Green;
+            }
+
+        }
+
+        private void btnDualAlt_Click(object sender, EventArgs e)
+        {
+            string strUser = Interaction.InputBox("Type G for green, other letters for orange");
+            if (strUser.ToUpper() == "G")
+            {
+                txtOutput.BackColor = Color.Green;
+            }
+            else
+            {
+                txtOutput.BackColor = Color.Orange;
+            }
+        }
+
+        private void btnMultiAlt_Click(object sender, EventArgs e)
+        {
+            string strUser = Interaction.InputBox(
+                "Type G for green, O for orange, V for Violet, R for Red," + "Y for Yellow, other letters for white.");
+            if (strUser.ToUpper() == "G")
+            {
+                txtOutput.BackColor = Color.Green;
+            }
+            else if (strUser.ToUpper() == "O")
+            {
+                txtOutput.BackColor = Color.Orange;
+            }
+            else if (strUser.ToUpper() == "V")
+            {
+                txtOutput.BackColor = Color.Violet;
+            }
+            else if (strUser.ToUpper() == "R")
+            {
+                txtOutput.BackColor = Color.Red;
+            }
+            else if (strUser.ToUpper() == "Y")
+            {
+                txtOutput.BackColor = Color.Yellow;
+            }
+            else
+            {
+                txtOutput.BackColor = Color.White;
+            }
+        }
+
+        private void btnCoupon_Click(object sender, EventArgs e)
+        {
+            string strUser = Interaction.InputBox("Do you have a coupon? Y for Yes and othe letters for No.");
+            if (strUser.ToUpper() == "Y")
+            {
+                txtOutput.Text = "You get 10% discount with coupon. $180 please.";
+            }
+            else
+            {
+                txtOutput.Text = "$200 please.";
+            }
+        }
+
+        private void btnTryParse_Click(object sender, EventArgs e)
+        {
+            byte bytX = 0;
+            string strInput = Interaction.InputBox("Type something and see what the function Byte.TryParse do.");
+            if (byte.TryParse(strInput, out bytX))
+            {
+                txtOutput.Text = "Byte.TryParse function returns " + byte.TryParse(strInput, out bytX) +
+                    " becasue \"" + strInput + "\" can be converted into a Byte number. " + "At the same time. " +
+                    bytX + " is assigned to the variable bytX.";
+            }
+            else
+            {
+                txtOutput.Text = "Byte.Tryparse function returns " + byte.TryParse(strInput, out bytX) +
+                    " becasue \"" + strInput + "\" cannot be converted into a Byte number. " + "At the same time, " +
+                    bytX + " is assigned to the variable bytX.";
+            }
+        }
+
+        private void btnDriverLic_Click(object sender, EventArgs e)
+        {
+            byte bytAge = 0;
+            string strAge = Interaction.InputBox("How old are you?" + Constants.vbNewLine + "If you are 18 tomorrow, you should answer 17.");
+            if (byte.TryParse(strAge, out bytAge) == false)
+            {
+                txtOutput.Text = "Please enter a reasonable integer number for age." + strAge + " is not correct. Try Again.";
+                return;
+            }
+            if (bytAge >= 18)
+            {
+                txtOutput.Text = "You may apply for a driver license.";
+            }
+            else if (bytAge >= 16)
+            {
+                txtOutput.Text = "You may apply for a driving permit, and drive under supervision.";
+            }
+            else
+            {
+                txtOutput.Text = "You are too young to drive.";
+            }
+        }
+
+        private void btnTicket_Click(object sender, EventArgs e)
+        {
+            string strUser = Interaction.InputBox(
+                "Type S for senior, C for child, other letter for anyone else,");
+            if (strUser.ToUpper() == "S")
+            {
+                txtOutput.Text = "Five dollars for a senior.";
+            }
+            else if (strUser.ToUpper() == "C")
+            {
+                txtOutput.Text = "Free for Children.";
+            }
+            else
+            {
+                txtOutput.Text = "Pay $10.00 please.";
+            }
+        }
+
+        private void btnVending_Click(object sender, EventArgs e)
+        {
+            string strUser = Interaction.InputBox(
+                "Tyep 10 for Pepsi; 15 for Coca-Cola; 18 for Snapple; 22 for Ginger Ale; 30 for Gatorade");
+            if (strUser == "10")
+            {
+                txtOutput.Text = "You get a Pepsi.";
+            }
+            else if (strUser == "15")
+            {
+                txtOutput.Text = "You get a Cola-Cola";
+            }
+            else if (strUser == "18")
+            {
+                txtOutput.Text = "You get a Snapple";
+            }
+            else if (strUser == "22")
+            {
+                txtOutput.Text = "You get a Ginger Ale";
+
+            }
+            else if (strUser == "130")
+            {
+                txtOutput.Text = "You get a Gatorade";
+            }
+            else
+            {
+                txtOutput.Text = strUser + " is a wrong choice. Try Again.";
+            }
+
+        }
+    }
+}
+   
